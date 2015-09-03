26e6c2bc845993d01815fa9f6ee08fdaa708da63
Raabb Ajam <raabbajam@gmail.com>
2015-09-04 06:40:22 +0700

    Added xtoken on Garuda

--- modules/garuda.js
+++ modules/garuda.js
@@ -203,6 +203,7 @@ function generateData(id) {
 		cek_instant_id: cek_instant_id,
 		dep_radio: cek_instant_id,
 		priceScraper: false
+		xToken: this._dt.xToken,
 	};
 	for (var i = 5, j = 1, ln = _id.length; i < ln; i++, j++) {
 		data['transit' + j] = _id[i];
